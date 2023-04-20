38c3561706633f2953557585e50a21c3109e3eb0
srilakshmi.g <srilakshmi210799@gmail.com>
2023-04-20 14:09:37 +0530

    mongod ip adress change

--- mongodb.sh
+++ mongodb.sh
@@ -3,4 +3,5 @@ yum install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
 ## here we need to change local host 127.0.0.1 with 0.0.0.0
+sed -i -e 's|127.0.0.0|0.0.0.0|' passwd
 systemctl restart mongod
