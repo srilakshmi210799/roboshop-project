49173d8e720048f3bce6966907e5a892f3bc0721
srilakshmi.g <srilakshmi210799@gmail.com>
2023-04-22 16:26:40 +0530

    catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -13,7 +13,7 @@ mkdir /app
 
 echo -e "\e[36m<<<<<<<<download app content>>>>>>>>>>\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-cd /app
+
 
 echo -e "\e[36m<<<<<<<<<<<unzip app content>>>>>>>>>>\e[0m"
 unzip /tmp/catalogue.zip
@@ -22,13 +22,15 @@ echo -e "\e[36m<<<<<<<<install nodejs dependencies>>>>>>>>>\e[0m"
 npm install
 
 echo -e "\e[36m<<<<<<<copy catalogue systemd file>>>>>>>\e[0m"
-cp home/centos/roboshop-project/catalogue.service /etc/systemd/system/catalogue.service
+cp catalogue.service /etc/systemd/system/catalogue.service
+
 echo -e "\e[36m<<<<<<<<<<start catalogue service>>>>>>>>\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
+
 echo -e "\e[36m<<<<<<<<<<<<<copy mongodb repo>>>>>>>>>>\e[0m"
-cp /home/centos/roboshop-project/mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 echo -e "\e[36m<<<<<<<<<<<< install mongodb client>>>>>>>>>>>>>>>\e[0m"
 yum install mongodb-org-shell -y
