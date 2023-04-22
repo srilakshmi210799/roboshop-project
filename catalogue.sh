d16e245bca0021c4d75d0d3c196d2ddaf85f06f5
srilakshmi.g <srilakshmi210799@gmail.com>
2023-04-22 15:28:55 +0530

    catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -5,6 +5,7 @@ yum install nodejs -y
 echo -e "\e[36m<<<<<<<<create application user>>>>>>\e[0m"
 useradd roboshop
 echo -e "\e[36m<<<<<<<<<<create application directory>>>>>>>>>\e[0m"
+rm -rf app
 mkdir /app
 echo -e "\e[36m<<<<<<<<download app content>>>>>>>>>>\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
@@ -15,13 +16,13 @@ cd /app
 echo -e "\e[36m<<<<<<<<install nodejs dependencies>>>>>>>>>\e[0m"
 npm install
 echo -e "\e[36m<<<<<<<copy catalouge systemd file>>>>>>>\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service
+cp /home/centos/roboshop-project/catalogue.service /etc/systemd/system/catalogue.service
 echo-e "\e[36m<<<<<<<<<<start catalouge service>>>>>>>>\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
 echo -e "\e[36m<<<<<<<<<<<<<copy mongodb repo>>>>>>>>>>\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp /home/centos/roboshop-project/mongo.repo /etc/yum.repos.d/mongo.repo
 echo -e "\e[36m<<<<<<<<<<<< install mongodb client>>>>>>>>>>>>>>>\e[0m"
 yum install mongodb-org-shell -y
 echo -e "\e[36m<<<<<<<<load schema>>>>>>>>\e[0m"
